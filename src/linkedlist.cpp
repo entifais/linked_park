af9939175c0715e1125e067ff76bb88fd4b0defa
jero98772 <jero98772@protonmail.com>
2022-09-18 20:58:45 -0500

    update

--- src/linkedlist.cpp
+++ src/linkedlist.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
 #include "linkedlist.h"
+#include <iostream>
+#include <string>
 
 node::node(){}
 node::node(string d1,string d2){
@@ -36,10 +37,11 @@ linkedlist::del(int pos){
 	Node *tmp1=this->head;
 	int i=0;
 	if(this->head==NULL){
+		cout<<"[ERROR] can't remove node from an empty linked list\nadds items to the list"<<endl;
 		return;
-		//error
 	}
 	if(pos>i;){
+		cout<<"[ERROR] IndexError: list index out of range\nadds items to the list"<<endl;
 		return;
 		//error
 	}
@@ -54,3 +56,10 @@ linkedlist::del(int pos){
 	return this->name
 }
 
+linkedlist::print(){
+	this->head=NULL;
+}
+
+linkedlist::printWithStyle(){
+	this->head=NULL;
+}
